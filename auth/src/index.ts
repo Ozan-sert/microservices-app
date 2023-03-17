f3e64a1c1c5d7e63d30b0377b81f612d5f2ef9a5
Ozan-sert <ozansert1993@gmail.com>
2023-03-17 00:54:06 -0400

    added jwt to auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,9 +8,15 @@ import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handlers";
 import { NotFoundError } from "./errors/not-found-error";
+import cookieSession from "cookie-session";
 
 const app = express();
+app.set('trust proxy', true)
 app.use(json());
+app.use(cookieSession({
+  signed: false
+})
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
