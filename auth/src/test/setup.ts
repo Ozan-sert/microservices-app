93baf99cadb53f57d78d444918dda8ef2849fb43
Ozan-sert <ozansert1993@gmail.com>
2023-03-17 21:31:44 -0400

    Implement test for current-user function

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,16 +1,20 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from 'supertest';
+import { app } from "../app";
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asdfasdf';
-  
-  
-   mongo = await MongoMemoryServer.create();
+  process.env.JWT_KEY = "asdfasdf";
+
+  mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
-
-  await mongoose.connect(mongoUri, { });
+  await mongoose.connect(mongoUri, {});
 });
 
 beforeEach(async () => {
@@ -27,3 +31,20 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+  
+    const response = await request(app)
+      .post('/api/users/signup')
+      .send({
+        email,
+        password
+      })
+      .expect(201);
+  
+    const cookie = response.get('Set-Cookie');
+  
+    return cookie;
+  };
