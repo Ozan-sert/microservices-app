687d52735971b78d2ab9f8a385f9d8f65ede04d5
Ozan-sert <ozansert1993@gmail.com>
2023-03-19 03:15:03 -0400

    Implement tickets service routes and tests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,20 +1,32 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
-import { errorHandler, NotFoundError } from "@osticketsmod/common";
 import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@osticketsmod/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
-app.set('trust proxy', true)
+app.set("trust proxy", true);
 app.use(json());
-app.use(cookieSession({
-  signed: false
-})
+app.use(
+  cookieSession({
+    signed: false,
+  })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
+
 app.use(errorHandler);
 
-export { app }
+export { app };
